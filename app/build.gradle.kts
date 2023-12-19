0182ad783c401e24fa0e59cdb6572ad2b0e59987
Sandhy <sandhys66@gmail.com>
2023-12-20 00:59:26 +0700

    Added CameraX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        mlModelBinding = true
     }
 
 }
@@ -59,6 +60,9 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("androidx.camera:camera-lifecycle:1.3.1")
+    implementation("org.tensorflow:tensorflow-lite-support:0.1.0")
+    implementation("org.tensorflow:tensorflow-lite-metadata:0.1.0")
+    implementation("org.tensorflow:tensorflow-lite-gpu:2.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -69,6 +73,11 @@ dependencies {
     implementation("androidx.camera:camera-view:1.3.1")
     implementation("androidx.camera:camera-extensions:1.3.1")
 
+    implementation("org.tensorflow:tensorflow-lite-support:0.4.4")
+    implementation("org.tensorflow:tensorflow-lite-metadata:0.4.4")
+    implementation("org.tensorflow:tensorflow-lite-gpu:2.13.0")
+    implementation("org.tensorflow:tensorflow-lite-gpu-delegate-plugin:0.4.4")
+    implementation("org.tensorflow:tensorflow-lite-task-vision:0.4.4")
 
     implementation("de.hdodenhof:circleimageview:3.1.0")
 
--- app/src/main/java/bangkit/project/fed/ui/captureegg/camera/CameraActivity.kt
+++ app/src/main/java/bangkit/project/fed/ui/captureegg/camera/CameraActivity.kt
@@ -10,6 +10,7 @@ import androidx.camera.core.CameraControl
 import androidx.camera.core.CameraInfoUnavailableException
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.FocusMeteringAction
+import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
@@ -18,8 +19,10 @@ import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import bangkit.project.fed.databinding.ActivityCameraBinding
 import bangkit.project.fed.ui.captureegg.imagedisplay.ImageDisplayActivity
+import org.tensorflow.lite.task.vision.classifier.Classifications
 import java.io.File
 import java.text.NumberFormat
+import java.util.concurrent.Executors
 
 class CameraActivity : AppCompatActivity() {
 
@@ -27,6 +30,7 @@ class CameraActivity : AppCompatActivity() {
     private lateinit var imageCapture: ImageCapture
     private lateinit var preview: Preview
     private lateinit var gestureDetector: GestureDetector
+    private lateinit var imageClassifierHelper: ImageClassifierHelper
 
     private var flashEnabled = false
     private var cameraControl : CameraControl? = null
@@ -58,6 +62,36 @@ class CameraActivity : AppCompatActivity() {
     }
 
     private fun startCameraCapture() {
+
+        imageClassifierHelper =
+            ImageClassifierHelper(
+                context = this,
+                imageClassifierListener = object : ImageClassifierHelper.ClassifierListener {
+                    override fun onError(error: String) {
+                        runOnUiThread {
+                            Toast.makeText(this@CameraActivity, error, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    override fun onResults(results: List<Classifications>?, inferenceTime: Long) {
+                        runOnUiThread {
+                            results?.let { it ->
+                                if (it.isNotEmpty() && it[0].categories.isNotEmpty()) {
+                                    println(it)
+                                    val sortedCategories =
+                                        it[0].categories.sortedByDescending { it?.score }
+                                    val displayResult =
+                                        sortedCategories.joinToString("\n") {
+                                            "${it.label} " + NumberFormat.getPercentInstance().format(it.score).trim()
+                                        }
+                                    binding.detectionText.text = displayResult
+                                }
+                            }
+                        }
+                    }
+                })
+
+
         // Get the CameraProvider
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
@@ -69,15 +103,27 @@ class CameraActivity : AppCompatActivity() {
             preview = Preview.Builder().build()
             preview.setSurfaceProvider(binding.preview.surfaceProvider)
 
+            val imageAnalyzer = ImageAnalysis.Builder()
+                .setTargetRotation(binding.preview.display.rotation)
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+                .build()
+                .also {
+                    it.setAnalyzer(Executors.newSingleThreadExecutor()) {image ->
+                        imageClassifierHelper.classify(image)
+                    }
+                }
+
             // Set up the image capture use case
             imageCapture = ImageCapture.Builder()
                 .setTargetRotation(windowManager.defaultDisplay.rotation)
                 .build()
 
             try {
+                cameraProvider.unbindAll()
                 // Connect the preview use case to the Camera
                 cameraProvider.bindToLifecycle(
-                    this, CameraSelector.DEFAULT_BACK_CAMERA, preview
+                    this, CameraSelector.DEFAULT_BACK_CAMERA, preview, imageAnalyzer
                 )
 
                 // Now, bind the ImageCapture use case to the Camera
@@ -151,5 +197,18 @@ class CameraActivity : AppCompatActivity() {
             e.printStackTrace()
         }
     }
+
+        override fun onPause() {
+        super.onPause()
+        imageClassifierHelper.clearImageClassifier()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (imageClassifierHelper.isClosed()) {
+            imageClassifierHelper.setupImageClassifier()
+        }
+    }
+
 
 }
