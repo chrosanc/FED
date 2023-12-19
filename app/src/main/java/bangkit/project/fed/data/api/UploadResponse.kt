80bf86ada8a81324cf011000eebe1edca62e1edd
Zulfahmi M. Ardianto <zulfahmimaulana3@gmail.com>
2023-12-19 14:34:53 +0700

    upload

---
+++ app/src/main/java/bangkit/project/fed/data/api/UploadResponse.kt
@@ -0,0 +1,10 @@
+package bangkit.project.fed.data.api
+
+import com.google.gson.annotations.SerializedName
+
+data class UploadResponse(
+    @field:SerializedName("error")
+    val error: Boolean,
+    @field:SerializedName("message")
+    val message: String
+)
