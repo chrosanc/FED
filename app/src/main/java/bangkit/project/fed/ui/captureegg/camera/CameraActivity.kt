89abf869092ab1e7ea3ba06c212c65837860a3a7
Sandhy <sandhys66@gmail.com>
2023-12-20 21:58:43 +0700

    Now theres two mode on camera , CameraFragment and RealtimeFragment

--- app/src/main/java/bangkit/project/fed/ui/captureegg/camera/CameraActivity.kt
+++ app/src/main/java/bangkit/project/fed/ui/captureegg/camera/CameraActivity.kt
@@ -19,6 +19,7 @@ import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import bangkit.project.fed.databinding.ActivityCameraBinding
 import bangkit.project.fed.ui.captureegg.imagedisplay.ImageDisplayActivity
+import com.google.android.material.tabs.TabLayoutMediator
 import com.google.common.util.concurrent.ListenableFuture
 import org.tensorflow.lite.task.vision.classifier.Classifications
 import java.io.File
@@ -28,186 +29,25 @@ import java.util.concurrent.Executors
 class CameraActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCameraBinding
-    private lateinit var imageCapture: ImageCapture
-    private lateinit var preview: Preview
-    private lateinit var gestureDetector: GestureDetector
-    private lateinit var imageClassifierHelper: ImageClassifierHelper
-    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
-
-    private var flashEnabled = false
-    private var cameraControl : CameraControl? = null
+    private lateinit var viewPagerAdapter: CameraPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCameraBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        gestureDetector = GestureDetector(this, GestureListener())
+        viewPagerAdapter = CameraPagerAdapter(supportFragmentManager, lifecycle)
+        binding.viewPager.adapter = viewPagerAdapter
 
-        binding.preview.setOnTouchListener { _, event ->
-            gestureDetector.onTouchEvent(event)
-            if (event.action == MotionEvent.ACTION_UP) {
-                binding.preview.performClick()
+        TabLayoutMediator(binding.tabLayout, binding.viewPager){tab, position ->
+            when (position) {
+                0 -> tab.text = "Camera"
+                1 -> tab.text = "Realtime"
             }
-            true
-        }
+        }.attach()
 
-        binding.flashlight.setOnClickListener {
-            toggleFlash()
-        }
 
-        binding.capture.setOnClickListener {
-            takePicture()
-        }
 
-        startCameraCapture()
     }
-
-    private fun startCameraCapture() {
-
-        imageClassifierHelper =
-            ImageClassifierHelper(
-                context = this,
-                imageClassifierListener = object : ImageClassifierHelper.ClassifierListener {
-                    override fun onError(error: String) {
-                        runOnUiThread {
-                            Toast.makeText(this@CameraActivity, error, Toast.LENGTH_SHORT).show()
-                        }
-                    }
-
-                    override fun onResults(results: List<Classifications>?, inferenceTime: Long) {
-                        runOnUiThread {
-                            results?.let { it ->
-                                if (it.isNotEmpty() && it[0].categories.isNotEmpty()) {
-                                    println(it)
-                                    val sortedCategories =
-                                        it[0].categories.sortedByDescending { it?.score }
-                                    val displayResult =
-                                        sortedCategories.joinToString("\n") {
-                                            "${it.label} " + NumberFormat.getPercentInstance().format(it.score).trim()
-                                        }
-                                    binding.detectionText.text = displayResult
-                                }
-                            }
-                        }
-                    }
-                })
-
-
-        // Get the CameraProvider
-        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-
-        cameraProviderFuture.addListener({
-            // CameraProvider is now ready
-            val cameraProvider = cameraProviderFuture.get()
-
-            // Set up the preview
-            preview = Preview.Builder().build()
-            preview.setSurfaceProvider(binding.preview.surfaceProvider)
-
-            val imageAnalyzer = ImageAnalysis.Builder()
-                .setTargetRotation(windowManager.defaultDisplay.rotation)
-                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
-                .build()
-                .also {
-                    it.setAnalyzer(Executors.newSingleThreadExecutor()) {image ->
-                        imageClassifierHelper.classify(image)
-                    }
-                }
-
-            // Set up the image capture use case
-            imageCapture = ImageCapture.Builder()
-                .setTargetRotation(windowManager.defaultDisplay.rotation)
-                .build()
-
-            try {
-                cameraProvider.unbindAll()
-                // Connect the preview use case to the Camera
-                cameraProvider.bindToLifecycle(
-                    this, CameraSelector.DEFAULT_BACK_CAMERA, preview, imageAnalyzer
-                )
-
-                // Now, bind the ImageCapture use case to the Camera
-                cameraProvider.bindToLifecycle(
-                    this, CameraSelector.DEFAULT_BACK_CAMERA, imageCapture
-                )
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }, ContextCompat.getMainExecutor(this))
-    }
-
-    private fun toggleFlash() {
-        flashEnabled = !flashEnabled
-        imageCapture.flashMode =
-            if (flashEnabled) {
-                ImageCapture.FLASH_MODE_ON
-            } else {
-                ImageCapture.FLASH_MODE_OFF
-            }
-    }
-
-    private fun takePicture() {
-        val photoFile = createTemporaryFile("picture", ".jpg")
-        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
-
-        imageCapture.takePicture(
-            outputOptions, ContextCompat.getMainExecutor(this),
-            object : ImageCapture.OnImageSavedCallback {
-                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
-                    val mImageUri =
-                        FileProvider.getUriForFile(this@CameraActivity, "bangkit.project.fed.provider", photoFile)
-                    val intent = Intent(this@CameraActivity, ImageDisplayActivity::class.java)
-                    intent.putExtra("capturedImage", mImageUri)
-                    startActivity(intent)
-                }
-
-                override fun onError(exception: ImageCaptureException) {
-                    exception.printStackTrace()
-                }
-            }
-        )
-    }
-
-    private fun createTemporaryFile(part: String, ext: String): File {
-        val tempDir = filesDir
-        if (!tempDir.exists()) {
-            tempDir.mkdirs()
-        }
-        return File.createTempFile(part, ext, tempDir)
-    }
-
-    private inner class GestureListener : GestureDetector.SimpleOnGestureListener() {
-        override fun onSingleTapConfirmed(e: MotionEvent): Boolean {
-            e?.let {
-                handleTapToFocus(e.x, e.y)
-                return true
-            }
-            return false
-        }
-    }
-
-    private fun handleTapToFocus(x: Float, y: Float) {
-        val meteringPointFactory = binding.preview.meteringPointFactory
-        val autoFocusPoint = meteringPointFactory.createPoint(x, y)
-
-        try {
-            val action = FocusMeteringAction.Builder(autoFocusPoint).build()
-            cameraControl?.startFocusAndMetering(action)
-        } catch (e: CameraInfoUnavailableException) {
-            e.printStackTrace()
-        }
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        cameraProviderFuture.addListener({
-            val cameraProvider = cameraProviderFuture.get()
-            cameraProvider.unbindAll()
-            imageClassifierHelper.clearImageClassifier()
-            // Release additional TensorFlow Lite resources if needed.
-        }, ContextCompat.getMainExecutor(this))
-    }
-
 
 }
--- app/src/main/java/bangkit/project/fed/ui/captureegg/camera/CameraFragment.kt
+++ app/src/main/java/bangkit/project/fed/ui/captureegg/camera/CameraFragment.kt
@@ -1,60 +1,127 @@
 package bangkit.project.fed.ui.captureegg.camera
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import bangkit.project.fed.R
+import bangkit.project.fed.databinding.FragmentCameraBinding
+import bangkit.project.fed.ui.captureegg.imagedisplay.ImageDisplayActivity
+import com.google.common.util.concurrent.ListenableFuture
+import java.io.File
+import java.util.concurrent.Executors
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CameraFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CameraFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding : FragmentCameraBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var imageCapture: ImageCapture
+    private lateinit var preview: Preview
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_camera, container, false)
+        _binding = FragmentCameraBinding.inflate(inflater,container, false)
+
+
+        binding.flashlight.setOnClickListener {
+            toggleFlash()
+        }
+
+        binding.capture.setOnClickListener {
+            takePicture()
+        }
+
+        startCameraCapture()
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CameraFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CameraFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun toggleFlash() {
+
+    }
+
+    private fun takePicture() {
+        val photoFile = createTemporaryFile("picture", ".jpg")
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        imageCapture.takePicture(
+            outputOptions, ContextCompat.getMainExecutor(requireContext()),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val mImageUri =
+                        FileProvider.getUriForFile(requireContext(), "bangkit.project.fed.provider", photoFile)
+                    val intent = Intent(requireContext(), ImageDisplayActivity::class.java)
+                    intent.putExtra("capturedImage", mImageUri)
+                    startActivity(intent)
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    exception.printStackTrace()
                 }
             }
+        )
     }
+
+    private fun startCameraCapture() {
+        cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+            // CameraProvider is now ready
+            val cameraProvider = cameraProviderFuture.get()
+
+            // Set up the preview
+            preview = Preview.Builder().build()
+            preview.setSurfaceProvider(binding.preview.surfaceProvider)
+
+            // Set up the image capture use case
+            imageCapture = ImageCapture.Builder()
+                .setTargetRotation(activity?.windowManager!!.defaultDisplay.rotation)
+                .build()
+
+            try {
+                cameraProvider.unbindAll()
+                // Connect the preview use case to the Camera
+                cameraProvider.bindToLifecycle(
+                    this, CameraSelector.DEFAULT_BACK_CAMERA, preview
+                )
+
+                // Now, bind the ImageCapture use case to the Camera
+                cameraProvider.bindToLifecycle(
+                    this, CameraSelector.DEFAULT_BACK_CAMERA, imageCapture
+                )
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
+
+    }
+
+    private fun createTemporaryFile(part: String, ext: String): File {
+        val tempDir = activity?.filesDir
+        if (tempDir != null) {
+            if (!tempDir.exists()) {
+                tempDir.mkdirs()
+            }
+        }
+        return File.createTempFile(part, ext, tempDir)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startCameraCapture()
+    }
+
 }
