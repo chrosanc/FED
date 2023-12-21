ee6f438cd750cb16720641931eb8b09bf6509dfa
Sandhy <sandhys66@gmail.com>
2023-12-21 13:37:37 +0700

    Now theres two mode on camera , CameraFragment and RealtimeFragment

--- app/src/main/java/bangkit/project/fed/ui/captureegg/camera/CameraActivity.kt
+++ app/src/main/java/bangkit/project/fed/ui/captureegg/camera/CameraActivity.kt
@@ -1,30 +1,14 @@
 package bangkit.project.fed.ui.captureegg.camera
 
-import android.content.Intent
+import android.content.DialogInterface
 import android.os.Bundle
-import android.view.GestureDetector
-import android.view.MotionEvent
-import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.CameraControl
-import androidx.camera.core.CameraInfoUnavailableException
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.FocusMeteringAction
-import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
-import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.core.content.ContextCompat
-import androidx.core.content.FileProvider
 import bangkit.project.fed.databinding.ActivityCameraBinding
-import bangkit.project.fed.ui.captureegg.imagedisplay.ImageDisplayActivity
 import com.google.android.material.tabs.TabLayoutMediator
-import com.google.common.util.concurrent.ListenableFuture
+import org.tensorflow.lite.gpu.CompatibilityList
+import org.tensorflow.lite.gpu.GpuDelegate
 import org.tensorflow.lite.task.vision.classifier.Classifications
-import java.io.File
-import java.text.NumberFormat
-import java.util.concurrent.Executors
 
 class CameraActivity : AppCompatActivity() {
 
@@ -39,15 +23,54 @@ class CameraActivity : AppCompatActivity() {
         viewPagerAdapter = CameraPagerAdapter(supportFragmentManager, lifecycle)
         binding.viewPager.adapter = viewPagerAdapter
 
-        TabLayoutMediator(binding.tabLayout, binding.viewPager){tab, position ->
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
             when (position) {
                 0 -> tab.text = "Camera"
                 1 -> tab.text = "Realtime"
             }
         }.attach()
 
-
+        if (binding.viewPager.currentItem == 1) {
+            checkDeviceSupport()
+        }
 
     }
 
+    private fun checkDeviceSupport() {
+        if (CompatibilityList().isDelegateSupportedOnThisDevice) {
+            val imageClassifierHelper = ImageClassifierHelper(
+                context = this,
+                currentDelegate = ImageClassifierHelper.DELEGATE_GPU,
+                imageClassifierListener = object : ImageClassifierHelper.ClassifierListener {
+                    override fun onError(error: String) {
+                        // Handle the error and show the alert dialog
+                        showAlertDialog("Error", error)
+
+                        // If GPU is not supported, switch back to the Camera fragment (position 0)
+                        binding.viewPager.currentItem = 0
+                    }
+
+                    override fun onResults(results: List<Classifications>?, inferenceTime: Long) {
+                        // Handle classification results if needed
+                    }
+                }
+            )
+
+            // Now, you can use the imageClassifierHelper for GPU-supported operations
+        } else {
+            // GPU is not supported, handle accordingly (show alert dialog, switch fragments, etc.)
+            showAlertDialog("Error", "GPU is not supported on this device")
+            binding.viewPager.currentItem = 0
+        }
+    }
+
+    private fun showAlertDialog(message: String, error: String) {
+        val builder = AlertDialog.Builder(this@CameraActivity)
+        builder.setTitle(message).setMessage(error)
+            .setPositiveButton("OK") { dialog: DialogInterface, _: Int -> dialog.dismiss() }
+        val alertDialog = builder.create()
+        alertDialog.show()
+    }
+
 }
+
